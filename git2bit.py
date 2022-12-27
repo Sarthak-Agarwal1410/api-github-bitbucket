18b63fd575d15a51bae30ce0c4cc4ad36bd6a59b
Sarthak Agarwal <69386523+Sarthak-Agarwal1410@users.noreply.github.com>
2022-12-27 12:56:13 +0530

    Update git2bit.py

--- git2bit.py
+++ git2bit.py
@@ -6,14 +6,13 @@
 import github3
 import bitbucket_api
 
-github_repo_url = 'https://github.com/Sarthak-Agarwal1410/repo'
-bitbucket_repo_url = 'https://bitbucket.org/Sarthak1410/repo'
-YOUR_GITHUB_TOKEN = 'a'
+github_repo_url = 'https://github.com/username/repo'
+bitbucket_repo_url = 'https://bitbucket.org/username/repo'
 
-gh = github3.login(token = YOUR_GITHUB_TOKEN)
-bb = bitbucket_api.Client(auth=('Sarthak1410', 'PASSWORD'))
+gh = github3.login(token = 'YOUR_GITHUB_TOKEN')
+bb = bitbucket_api.Client(auth=('username', 'PASSWORD'))
 
-github_repo = gh.repository(owner='Sarthak-Agarwal1410', repository='repo')
+github_repo = gh.repository(owner='username', repository='repo')
 branches = [branch.name for branch in github_repo.branches()]
 tags = [tag.name for tag in github_repo.tags()]
 prs = [pr for pr in github_repo.pull_requests()]
